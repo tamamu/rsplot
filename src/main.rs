88372013f1d20820684baf808731a709b413a45d
tamamu <tamamu.1r1s@gmail.com>
2019-01-03 19:02:50 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 
 extern crate rsplot;
-use rsplot::unit::{Point2D};
+use rsplot::unit::*;
 
 extern crate cairo;
-use cairo::{ImageSurface, Format, Context};
+use cairo::{ImageSurface, Format, Context, Matrix};
 
 use std::fs::File;
 
 fn main() {
-    let p = Point2D {x: 3., y: 4.};
-    println!("Hello, world!{:?}", p);
-    let surface = ImageSurface::create(Format::ARgb32, 600, 600)
+    let data = vec![
+        Point2D {x: 32., y: 40.},
+        Point2D {x: 50., y: 100.},
+    ];
+    let prop = Scatter2DProperty::new(data, 1.0, 1.0);
+    let mut plot = Plot2D::new(prop);
+    plot.width = 400;
+    plot.height = 400;
+    plot.x = 100.;
+    plot.y = 100.;
+    let surface = ImageSurface::create(Format::ARgb32, plot.width+200, plot.height+200)
         .expect("Couldn't create a surface!");
     let context = Context::new(&surface);
-    context.set_source_rgb(1.0, 0.0, 0.0);
+    context.set_source_rgb(1.0, 1.0, 1.0);
     context.paint();
+    context.set_source_rgb(0.0, 0.0, 0.0);
+    plot.render(&context);
 
     let mut file = File::create("output.png")
         .expect("Couldn't create a file!");
--- src/unit.rs
+++ src/unit.rs
@@ -1,5 +1,6 @@
 
 use std::f64::consts::PI;
+use std::cmp;
 
 extern crate cairo;
 use cairo::{ImageSurface, Format, Context};
@@ -20,18 +21,29 @@ pub struct Line2D {
 
 #[derive(Debug, Clone, Copy)]
 pub enum Color {
-    RGB(f32, f32, f32),
-    RGBA(f32, f32, f32, f32),
+    RGB(f64, f64, f64),
+    RGBA(f64, f64, f64, f64),
+}
+
+fn set_context_color(ctx: &Context, color: Color) {
+    match color {
+        Color::RGB(r, g, b) => {
+            ctx.set_source_rgb(r, g, b);
+        }
+        Color::RGBA(r, g, b, a) => {
+            ctx.set_source_rgba(r, g, b, a);
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
 pub struct BorderProperty {
     pub color: Color,
-    pub width: u32,
+    pub width: f64,
 }
 
 impl BorderProperty {
-    fn new(color: Color, width: u32) -> Self {
+    fn new(color: Color, width: f64) -> Self {
         Self {
             color: color,
             width: width,
@@ -46,6 +58,10 @@ pub struct Plot2D<T> {
     pub label_x: String,
     pub label_y: String,
     pub border: BorderProperty,
+    pub x: PlotUnit,
+    pub y: PlotUnit,
+    pub width: i32,
+    pub height: i32,
 }
 
 impl<T> Plot2D<T> {
@@ -55,7 +71,11 @@ impl<T> Plot2D<T> {
             title: "".to_string(),
             label_x: "".to_string(),
             label_y: "".to_string(),
-            border: BorderProperty::new(Color::RGB(0., 0., 0.), 1),
+            border: BorderProperty::new(Color::RGB(0., 0., 0.), 2.),
+            x: 0.,
+            y: 0.,
+            width: 800,
+            height: 800,
         }
     }
 }
@@ -65,14 +85,26 @@ pub struct Scatter2DProperty {
     pub data: Vec<Point2D>,
     pub unit_x: PlotUnit,
     pub unit_y: PlotUnit,
+    pub lim_x: (PlotUnit, PlotUnit),
+    pub lim_y: (PlotUnit, PlotUnit),
+    pub color: Color,
 }
 
 impl Scatter2DProperty {
     pub fn new(data: Vec<Point2D>, unit_x: PlotUnit, unit_y: PlotUnit) -> Self {
+        let mut lim_x = (0., 0.);
+        let mut lim_y = (0., 0.);
+        for p in &data {
+            lim_x = (p.x.min(lim_x.0), p.x.max(lim_x.1));
+            lim_y = (p.y.min(lim_y.0), p.y.max(lim_y.1));
+        }
         Self {
             data: data,
             unit_x: unit_x,
             unit_y: unit_y,
+            lim_x: lim_x,
+            lim_y: lim_y,
+            color: Color::RGB(0., 0., 0.),
         }
     }
 }
@@ -81,22 +113,50 @@ pub type Scatter2D = Plot2D<Scatter2DProperty>;
 
 
 pub trait Drawable {
-    fn draw(&self, ctx: &mut Context) {
+    fn draw(&self, ctx: &Context, width: f64, height: f64) {
         unimplemented!();
     }
 }
 
 impl<T: Drawable> Plot2D<T> {
-    pub fn render(&self, ctx: &mut Context) {
-        self.prop.draw(ctx);
+    pub fn render(&self, ctx: &Context) {
+        let w = self.width as f64;
+        let h = self.height as f64;
+        ctx.translate(self.x, self.y);
+        let color = self.border.color;
+        set_context_color(ctx, color);
+        ctx.set_line_width(self.border.width);
+        ctx.move_to(0., 0.);
+        ctx.line_to(w, 0.);
+        ctx.line_to(w, h);
+        ctx.line_to(0., h);
+        ctx.line_to(0., 0.);
+        ctx.stroke();
+        self.prop.draw(ctx, w, h);
     }
 }
 
 impl Drawable for Scatter2DProperty {
-    fn draw(&self, ctx: &mut Context) {
-        println!("{:?}", self.unit_x);
+    fn draw(&self, ctx: &Context, width: f64, height: f64) {
+        let ll_x = self.lim_x.0;
+        let ul_x = self.lim_x.1;
+        let ll_y = self.lim_y.0;
+        let ul_y = self.lim_y.1;
+        let ratio_x = width / (ul_x - ll_x);
+        let ratio_y = height / (ul_y - ll_y);
+        ctx.set_font_size(24.);
+        ctx.move_to(-50., 0.);
+        ctx.show_text(&ul_x.to_string());
+        ctx.move_to(-50., height);
+        ctx.show_text(&ll_x.to_string());
+        ctx.move_to(0., height+50.);
+        ctx.show_text(&ll_y.to_string());
+        ctx.move_to(width, height+50.);
+        ctx.show_text(&ul_y.to_string());
+        set_context_color(ctx, self.color);
         for p in &self.data {
-            ctx.arc(p.x, p.y, 4f64, 0., 2.*PI);
+            println!("{:?}", p);
+            ctx.arc((p.x-ll_x)*ratio_x, (ul_y-(p.y-ll_y))*ratio_y, 4f64, 0., 2.*PI);
             ctx.fill();
         }
     }
